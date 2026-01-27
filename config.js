a364ac630fbbb1815d7c7c81c8b99bd41862a3f7
emaanskindacutengl <all4emaanfatima@gmail.com>
2026-01-27 18:57:23 -0500

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you please be my Valentine? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW You love me that much?? 😝😘😘😝😝",  // Shows when they go past 5000%
+        high: "Okay thats better... 😑",              // Shows when they go past 1000%
+        normal: "That's it? 😞"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
