9f080903c90645a7f134636795b489328f68ceff
emaanskindacutengl <all4emaanfatima@gmail.com>
2026-01-27 18:53:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "I like you so so so so so much. Do you like me?",                                    // First interaction
+            text: "I like you more than you could ever imagine. Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
